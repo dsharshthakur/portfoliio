d06c2ea31b2922b1774a7053767b75f1a5975e5d
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2023-12-31 01:41:41 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -82,9 +82,9 @@ with st.container():
         
     <h4>Introducing Streamlit Gemini: Your AI-Powered Language Hub</h4>
     <br>
-    <br>
+    
     <h4>Harnessing the Power of AI for Effortless Translation and Summarization</h4.
-
+    <br>
     <p>Streamlit Gemini puts the power of language understanding and generation at your fingertips with an intuitive, user-friendly app. Built on the cutting-edge capabilities of Google Gemini and LangChain, it offers:</p>
     
     <b>Key Features:</b>
